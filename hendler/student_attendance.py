e8fd6e5ef4afb7d6a6c6025bb9bbd03d1bba23ff
Sourcery AI <>
2023-09-22 09:49:15 +0000

    'Refactored by Sourcery'

--- hendler/student_attendance.py
+++ hendler/student_attendance.py
@@ -14,12 +14,12 @@ class Student_Attendance:
 
     def add(self):
         for student in student_files:
-            student_data = data.read('.data/students-data/'+student)
+            student_data = data.read(f'.data/students-data/{student}')
             present_or_absent = input(f"{student_data['id']}    {student_data['first_name']} {student_data['last_name']}   is present? (P/A): ") or "A "
 
-            if present_or_absent == "p" or present_or_absent == "P":
+            if present_or_absent in ["p", "P"]:
                 present_or_absent = True
-            if present_or_absent == "a" or present_or_absent == "A":
+            if present_or_absent in ["a", "A"]:
                 present_or_absent = False
 
             present_data = {
@@ -30,78 +30,61 @@ class Student_Attendance:
                 for attendance in student_data['attendance']:
                     if attendance['date'] != date.strftime("%x"):
                         student_data['attendance'].append(present_data)
-                        data.update('.data/students-data/' + student, student_data)
+                        data.update(f'.data/students-data/{student}', student_data)
 
             else:
                 student_data['attendance'].append(present_data)
-                data.update('.data/students-data/' + student, student_data)
+                data.update(f'.data/students-data/{student}', student_data)
         print("Attendance Taken Complete")
 
     def read(self, student_id):
         global present
-        # student_id = input("Enter Student ID: ")
-
-        if student_id:
-            student_data = data.read('.data/students-data/' + student_id+'.json')
-            if type(student_data) == dict:
-                if len(student_data['attendance']) > 0:
-                    for attendance in student_data['attendance']:
-                        if attendance["is_present"]:
-                            present = "Present"
-                        elif not attendance["is_present"]:
-                            present = "Absent"
-
-                        print(f'{attendance["date"]} he/she is {present}')
-            else:
-                return "Student Not Found"
-        else:
+        if not student_id:
             return "Enter Student ID"
+        student_data = data.read(f'.data/students-data/{student_id}.json')
+        if type(student_data) != dict:
+            return "Student Not Found"
+        if len(student_data['attendance']) > 0:
+            for attendance in student_data['attendance']:
+                present = "Present" if attendance["is_present"] else "Absent"
+                print(f'{attendance["date"]} he/she is {present}')
 
     def update(self, student_id):
-        if student_id:
-            student_data = data.read('.data/students-data/' + student_id+'.json')
-            if type(student_data) == dict:
-
-                if len(student_data['attendance']) > 0:
-                    attendance_date = input("Enter Attendance Date (MM/DD/YY): ")
-                    for attendance in student_data['attendance']:
-                        if attendance['date'] == attendance_date:
-                            is_present = input("Enter he/she is present? (P/A): ")
-                            if is_present == "p" or is_present == "P":
-                                is_present = True
-                            if is_present == "a" or is_present == "A":
-                                is_present = False
-                            update_data = {
-                                "date": attendance['date'],
-                                "is_present": is_present
-                            }
-                            student_data['attendance'].remove(attendance)
-                            student_data['attendance'].append(update_data)
-                            data.update('.data/students-data/' + student_id+'.json', student_data)
-                            return "Attendance Updated"
-
-                else:
-                    return "No attendance found"
-            else:
-                return "Student Not Found"
-        else:
+        if not student_id:
             return "No Student ID Entered"
+        student_data = data.read(f'.data/students-data/{student_id}.json')
+        if type(student_data) != dict:
+            return "Student Not Found"
+        if len(student_data['attendance']) <= 0:
+            return "No attendance found"
+        attendance_date = input("Enter Attendance Date (MM/DD/YY): ")
+        for attendance in student_data['attendance']:
+            if attendance['date'] == attendance_date:
+                is_present = input("Enter he/she is present? (P/A): ")
+                if is_present in ["p", "P"]:
+                    is_present = True
+                if is_present in ["a", "A"]:
+                    is_present = False
+                update_data = {
+                    "date": attendance['date'],
+                    "is_present": is_present
+                }
+                student_data['attendance'].remove(attendance)
+                student_data['attendance'].append(update_data)
+                data.update(f'.data/students-data/{student_id}.json', student_data)
+                return "Attendance Updated"
 
     def delete(self):
-        student_id = input("Enter Student ID: ")
-        if student_id:
-            student_data = data.read('.data/students-data/' + student_id+'.json')
-            if type(student_data) == dict:
-                if len(student_data['attendance']) > 0:
-                    attendance_date = input("Enter Attendance Date (MM/DD/YY): ")
-                    for attendance in student_data['attendance']:
-                        if attendance['date'] == attendance_date:
-                            student_data['attendance'].remove(attendance)
-                            data.update('.data/students-data/' + student_id+'.json', student_data)
-                            return "Attendance Deleted"
-
-            else:
-                return "Student Not Found"
-        else:
+        if not (student_id := input("Enter Student ID: ")):
             return "Enter Student ID"
+        student_data = data.read(f'.data/students-data/{student_id}.json')
+        if type(student_data) != dict:
+            return "Student Not Found"
+        if len(student_data['attendance']) > 0:
+            attendance_date = input("Enter Attendance Date (MM/DD/YY): ")
+            for attendance in student_data['attendance']:
+                if attendance['date'] == attendance_date:
+                    student_data['attendance'].remove(attendance)
+                    data.update(f'.data/students-data/{student_id}.json', student_data)
+                    return "Attendance Deleted"
 
--- hendler/teacher_payments.py
+++ hendler/teacher_payments.py
@@ -11,128 +11,111 @@ class Teacher_Payments:
         self.month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     def add(self):
-        teacher_id = input("Enter Teacher Id: ")
-        if teacher_id:
-            teacher_data = data.read('.data/teachers-data/' + teacher_id + '.json')
-            if type(teacher_data) == dict:
-                teacher_payment = teacher_data['payments']
-
-                payment_date = input("Enter payment date format(18/08/2021): ")
-                payment_amount = int(input("Enter payment amount: "))
-                payment_month = int(input("Enter payment month Number: \n1. January\n2. February\n3. March\n4. April\n5. May\n6. June\n7. July\n8. August\n9. September\n10. October\n11. November\n12. December\n"))
-                is_payment = input("Is payment? (y/n): ")
-
-                if is_payment == 'y' or is_payment == "Y":
-                    is_payment = True
-                if is_payment == 'n' or is_payment == "N":
-                    is_payment = False
-
-                if payment_month and payment_date:
-
-                    payment_data = {
-                        "token": token,
-                        "date": payment_date,
-                        "amount": payment_amount,
-                        "month": self.month[payment_month - 1],
-                        "is_payment": is_payment
-                    }
-
-                    teacher_payment.append(payment_data)
-
-                    data.update('.data/teachers-data/' + teacher_id + '.json', teacher_data)
-                    return "\nPayment Successfully Added\n" + 'Token ID Number is: ' + str(token) + '\n' + 'calect Token ID Number'
-                else:
-                    return "Enter valid data"
-            else:
-                return "Teacher not found"
-        else:
+        if not (teacher_id := input("Enter Teacher Id: ")):
             return "Enter Teacher id"
+        teacher_data = data.read(f'.data/teachers-data/{teacher_id}.json')
+        if type(teacher_data) != dict:
+            return "Teacher not found"
+        teacher_payment = teacher_data['payments']
+
+        payment_date = input("Enter payment date format(18/08/2021): ")
+        payment_amount = int(input("Enter payment amount: "))
+        payment_month = int(input("Enter payment month Number: \n1. January\n2. February\n3. March\n4. April\n5. May\n6. June\n7. July\n8. August\n9. September\n10. October\n11. November\n12. December\n"))
+        is_payment = input("Is payment? (y/n): ")
+
+        if is_payment in ['y', "Y"]:
+            is_payment = True
+        if is_payment in ['n', "N"]:
+            is_payment = False
+
+        if payment_month and payment_date:
+
+            payment_data = {
+                "token": token,
+                "date": payment_date,
+                "amount": payment_amount,
+                "month": self.month[payment_month - 1],
+                "is_payment": is_payment
+            }
+
+            teacher_payment.append(payment_data)
+
+            data.update(f'.data/teachers-data/{teacher_id}.json', teacher_data)
+            return "\nPayment Successfully Added\n" + 'Token ID Number is: ' + str(token) + '\n' + 'calect Token ID Number'
+        else:
+            return "Enter valid data"
 
     def read(self, teacher_id):
         # teacher_id = input("Enter Teacher Id: ")
-        if teacher_id:
-            teacher_data = data.read('.data/teachers-data/' + teacher_id + '.json')
-            if type(teacher_data) == dict:
-                teacher_payment = teacher_data['payments']
-                for payment in teacher_payment:
-                    if payment['is_payment'] == True:
-                        clear_payment = "Paid"
-                    elif payment['is_payment'] == False:
-                        clear_payment = "Due"   
-                    print(f'payment date: {payment["date"]} payment amount: {payment["amount"]} payment month: {payment["month"]} is payment: {clear_payment}')
-                
-            else:
-                return "Teacher Not Registered"
-        else:
+        if not teacher_id:
             return "Enter Teacher id"
+        teacher_data = data.read(f'.data/teachers-data/{teacher_id}.json')
+        if type(teacher_data) != dict:
+            return "Teacher Not Registered"
+        teacher_payment = teacher_data['payments']
+        for payment in teacher_payment:
+            if payment['is_payment'] == True:
+                clear_payment = "Paid"
+            elif payment['is_payment'] == False:
+                clear_payment = "Due"   
+            print(f'payment date: {payment["date"]} payment amount: {payment["amount"]} payment month: {payment["month"]} is payment: {clear_payment}')
 
     def update(self, teacher_id):
         # teacher_id = input("Enter Teacher Id: ")
-        if teacher_id:
-            teacher_data = data.read('.data/teachers-data/' + teacher_id + '.json')
-
-            if type(teacher_data) == dict:
-                token_id = int(input("Enter token id: "))
-                teacher_payment = teacher_data['payments']
-
-                for payment in teacher_payment:
-                    if int(payment['token']) == token_id:
-                        find_token = payment
-
-                        payment_date = input("Enter payment date format(18/08/2021): ")
-                        payment_amount = input("Enter payment amount: ")
-                        payment_month = input("Enter payment month Number: \n1. January\n2. February\n3. March\n4. April\n5. May\n6. June\n7. July\n8. August\n9. September\n10. October\n11. November\n12. December\n")
-                        is_payment = input("Is payment? (y/n): ")
-
-                        if is_payment == 'y':
-                            is_payments = True
-                        if is_payment == 'n':
-                            is_payments = False
-
-                        if payment_date:
-                            find_token['date'] = payment_date
-                        else:
-                            find_token['date'] = find_token['date']
-
-                        if payment_month:
-                            find_token['month'] = self.month[int(payment_month) - 1]
-                        else:
-                            find_token['month'] = find_token['month']
-
-                        if payment_amount:
-                            find_token['amount'] = payment_amount
-                        else:
-                            find_token['amount'] = find_token['amount']
-
-                        if is_payment:
-                            find_token['is_payment'] = is_payments
-                        else:
-                            find_token['is_payment'] = find_token['is_payment']
-
-                        data.update('.data/teachers-data/' + teacher_id + '.json', teacher_data)
-                        return "Data Updated"
-
-            else:
-                return "Teacher Not Registered"
-        else:
+        if not teacher_id:
             return "Enter student id"
+        teacher_data = data.read(f'.data/teachers-data/{teacher_id}.json')
+
+        if type(teacher_data) != dict:
+            return "Teacher Not Registered"
+        token_id = int(input("Enter token id: "))
+        teacher_payment = teacher_data['payments']
+
+        for payment in teacher_payment:
+            if int(payment['token']) == token_id:
+                find_token = payment
+
+                payment_date = input("Enter payment date format(18/08/2021): ")
+                payment_amount = input("Enter payment amount: ")
+                payment_month = input("Enter payment month Number: \n1. January\n2. February\n3. March\n4. April\n5. May\n6. June\n7. July\n8. August\n9. September\n10. October\n11. November\n12. December\n")
+                is_payment = input("Is payment? (y/n): ")
+
+                if is_payment == 'n':
+                    is_payments = False
+
+                elif is_payment == 'y':
+                    is_payments = True
+                find_token['date'] = payment_date if payment_date else find_token['date']
+                find_token['month'] = (
+                    self.month[int(payment_month) - 1]
+                    if payment_month
+                    else find_token['month']
+                )
+                if payment_amount:
+                    find_token['amount'] = payment_amount
+                else:
+                    find_token['amount'] = find_token['amount']
+
+                if is_payment:
+                    find_token['is_payment'] = is_payments
+                else:
+                    find_token['is_payment'] = find_token['is_payment']
+
+                data.update(f'.data/teachers-data/{teacher_id}.json', teacher_data)
+                return "Data Updated"
 
     def delete(self, teacher_id):
         # teacher_id = input("Enter Teacher Id: ")
-        if teacher_id:
-            teacher_data = data.read('.data/teachers-data/' + teacher_id + '.json')
-            if type(teacher_id) == dict:
-
-                token_id = int(input("Enter token id: "))
-                teacher_payment = teacher_data['payments']
-
-                for payment in teacher_payment:
-                    if int(payment['token']) == token_id:
-                        teacher_payment.remove(payment)
-                        data.update('.data/teachers-data/' + teacher_id + '.json', teacher_data)
-                        return "Payment Data Deleted"
-
-            else:
-                return "Teacher Not Registered"
-        else:
+        if not teacher_id:
             return "Enter student id"
+        teacher_data = data.read(f'.data/teachers-data/{teacher_id}.json')
+        if type(teacher_id) != dict:
+            return "Teacher Not Registered"
+        token_id = int(input("Enter token id: "))
+        teacher_payment = teacher_data['payments']
+
+        for payment in teacher_payment:
+            if int(payment['token']) == token_id:
+                teacher_payment.remove(payment)
+                data.update(f'.data/teachers-data/{teacher_id}.json', teacher_data)
+                return "Payment Data Deleted"
